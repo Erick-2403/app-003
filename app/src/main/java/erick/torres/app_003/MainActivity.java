85e33f48d2328b652a4b5b12f179d1d55237e827
Erick-2403 <ericktorresjarrin@gmail.com>
2021-07-07 21:57:35 -0500

    Agregando los eventos

--- app/src/main/java/erick/torres/app_003/MainActivity.java
+++ app/src/main/java/erick/torres/app_003/MainActivity.java
@@ -3,6 +3,7 @@ package erick.torres.app_003;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Evento: onCreate", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"Evento: onStart", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"Evento: onStop", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Evento: onDestroy", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"Evento: onResume", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"Evento: onRestart", Toast.LENGTH_LONG).show();
     }
 }
